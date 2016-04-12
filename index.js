69e5c3cade04b5688dd7e45d35b17a0ddebea47a
Mickael Daniel <daniel.mickael@gmail.com>
2016-04-12 16:04:24 +0200

    Multiple -c options support

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var spawn = require('child_process').spawn;
 var alias = { c: 'cmd', h: 'help', v: 'version' };
 
 var opts = require('minimist')(process.argv.slice(2), { alias: alias });
+opts.cmd = Array.isArray(opts.cmd) ? opts.cmd : [opts.cmd];
+
 var files = opts._;
 
 if (opts.version) return console.log(require('./package').version);
@@ -19,11 +21,29 @@ if (!files.length) return console.error('Missing files argument');
 if (!opts.cmd) return console.error('Missing command option');
 
 require('gaze')(files, function(err, watcher) {
+
+  debug('Registering commands: %s', opts.cmd.join(', '));
+
   this.on('all', function(event, filepath) {
     if (!opts.cmd) return;
-    var command = opts.cmd.split(' ')[0];
-    var args = opts.cmd.split(' ').slice(1);
+
     debug(filepath + ' was ' + event);
-    spawn(command, args, { stdio: 'inherit' });
+
+    var cmds = opts.cmd.concat();
+    (function next(cmd) {
+      if (!cmd) return;
+
+      var command = cmd.split(' ')[0];
+      var args = cmd.split(' ').slice(1);
+
+      debug('`' + command + ' ' + args.join(' ') + '`');
+      spawn(command, args, { stdio: 'inherit' })
+        .on('error', function(err) {
+          console.error(err.stack);
+        })
+        .on('close', function() {
+          next(cmds.shift());
+        });
+    })(cmds.shift());
   });
 });
